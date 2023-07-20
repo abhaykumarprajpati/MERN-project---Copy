245c5bafbb6f6d84ac6597da70f8ad6cffa80721
abhaykumarprajapati <abhaya.prajapati@aarvytechnologies.co.in>
2023-07-20 10:32:10 +0530

    convert ifelse in switch in error js

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,40 +1,80 @@
 const ErrorHandler = require("../utils/errorhandler")
 
 
+// module.exports = (err, req, res, next) => {
+
+//     err.statusCode = err.statusCode || 500;
+//     err.message = err.message || "Internal Server Error";
+
+
+
+//     // Wrong Mongodb Id error
+//     if (err.name === "CastError") {
+//         const message = `Resource not found. Invalid: ${err.path}`;
+//         err = new ErrorHandler(message, 400);
+//     }
+//     //Mongoose duplicate key error
+//     if (err.code === 11000) {
+//         const message = ` Duplicate ${Object.keys(err.keyValue)} Entered`;
+//         err = new ErrorHandler(message, 400);
+//     }
+
+//     //wrong JWT error
+//     if (err.name === "JsonWebTokenError") {
+//         const message = ` Json Web Token is invalid, try again`;
+//         err = new ErrorHandler(message, 400);
+//     }
+
+//     //JWT expire error
+//     if (err.name === "TokenExpiredError") {
+//         const message = ` Json web Token is Expired , try again`;
+//         err = new ErrorHandler(message, 400);
+//     }
+
+
+
+//     res.status(err.statusCode).json({
+//         success: false,
+//         message: err.message
+//     });
+// };
+
+
+//same code using switch cases
+
+
+
 module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
+  switch (err.name) {
+    case "CastError":
+      const message = `Resource not found. Invalid: ${err.path}`;
+      err = new ErrorHandler(message, 400);
+      break;
 
+    case 11000:
+      const duplicateMessage = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+      err = new ErrorHandler(duplicateMessage, 400);
+      break;
 
+    case "JsonWebTokenError":
+      const jwtErrorMessage = "Json Web Token is invalid, try again";
+      err = new ErrorHandler(jwtErrorMessage, 400);
+      break;
 
-    // Wrong Mongodb Id error
-    if (err.name === "CastError") {
-        const message = `Resource not found. Invalid: ${err.path}`;
-        err = new ErrorHandler(message, 400);
-    }
-    //Mongoose duplicate key error
-    if (err.code === 11000) {
-        const message = ` Duplicate ${Object.keys(err.keyValue)} Entered`;
-        err = new ErrorHandler(message, 400);
-    }
+    case "TokenExpiredError":
+      const jwtExpiredMessage = "Json web Token is Expired, try again";
+      err = new ErrorHandler(jwtExpiredMessage, 400);
+      break;
 
-    //wrong JWT error
-    if (err.name === "JsonWebTokenError") {
-        const message = ` Json Web Token is invalid, try again`;
-        err = new ErrorHandler(message, 400);
-    }
+    default:
+      break;
+  }
 
-    //JWT expire error
-    if (err.name === "TokenExpiredError") {
-        const message = ` Json web Token is Expired , try again`;
-        err = new ErrorHandler(message, 400);
-    }
-
-
-
-    res.status(err.statusCode).json({
-        success: false,
-        message: err.message
-    });
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
 };
