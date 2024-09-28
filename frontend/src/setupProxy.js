7d3bd21931eae9d3c3e6606b7589e10e51f7258a
abhaykumarprajapati <abhaya.prajapati@aarvytechnologies.co.in>
2024-09-28 17:58:36 +0530

    changes proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = function (app) {
     app.use(
         // createProxyMiddleware("/api/v1/", { target: "http://localhost:5000/", secure: false, })
-        createProxyMiddleware("/api/v1/", { target: "mern-project-api-ruddy.vercel.app", secure: false, })
+        createProxyMiddleware("/api/v1/", { target: "https://mern-project-api-ruddy.vercel.app/", secure: false, })
     );
 };
 
