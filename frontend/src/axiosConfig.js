e18c5bbdaee25014d7739fb0d62dcb8e6830da65
abhaykumarprajapati <abhaya.prajapati@aarvytechnologies.co.in>
2024-09-29 12:39:20 +0530

    changes

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,15 +1,13 @@
-// src/axiosConfig.js
 
 import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // Use an environment variable for the base URL
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    'Content-Type': 'application/json', // Set default headers if needed
+    'Content-Type': 'application/json',
   },
 });
 
-// You can set up interceptors for requests or responses here if neededm
 
 export default axiosInstance;
